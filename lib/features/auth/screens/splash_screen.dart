62a062bcc134d8133ca4f0c011c58ceb45086ddb
mauriceezechieldegnon-wq <mauriceezechieldegnon@gmail.com>
2026-05-02 10:31:57 +0200

    Update splash_screen.dart

--- lib/features/auth/screens/splash_screen.dart
+++ lib/features/auth/screens/splash_screen.dart
@@ -69,7 +69,7 @@ class _WizzySplashScreenState extends State<WizzySplashScreen> {
                     ),
                     const SizedBox(height: 25),
                     const Text(
-                      "WIZZY V3",
+                      " WIZZY 1 ",
                       style: TextStyle(
                         color: Colors.white,
                         fontSize: 36,
