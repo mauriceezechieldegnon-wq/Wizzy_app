50c027e886dfacf56f3f100bea1e62f63f1406a0
mauriceezechieldegnon-wq <mauriceezechieldegnon@gmail.com>
2026-06-08 12:18:52 +0200

    Fix Firebase options for Windows and macOS
    Added support for macOS in Firebase options.

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,17 +1,14 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
-import 'package:flutter/foundation.dart'
-    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) return web;
     switch (defaultTargetPlatform) {
-      case TargetPlatform.android:
-        return android;
-      case TargetPlatform.windows: // ACTIVÉ ✅
-        return web; 
-      default:
-        throw UnsupportedError('Platform not supported');
+      case TargetPlatform.android: return android;
+      case TargetPlatform.windows: return web; // Fix Windows
+      case TargetPlatform.macOS: return web;   // Fix Mac
+      default: throw UnsupportedError('Platform not supported');
     }
   }
 
