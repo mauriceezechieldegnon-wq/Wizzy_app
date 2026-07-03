6795d7f7753cc8b8cb547a745de243c4153851e9
mauriceezechieldegnon-wq <mauriceezechieldegnon@gmail.com>
2026-07-03 07:36:53 +0100

    Add enums for MessageStatus and MessageType

--- lib/features/messenger/models/message_model.dart
+++ lib/features/messenger/models/message_model.dart
@@ -1,31 +1,52 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+enum MessageStatus { sent, delivered, read }
+enum MessageType { text, image, video, audio, file }
+
 class MessageModel {
+  final String id;
   final String senderId;
   final String text;
+  final MessageType type;
+  final String? mediaUrl;
+  final MessageStatus status;
   final Timestamp timestamp;
+  final Map<String, dynamic>? replyTo; 
 
   MessageModel({
+    required this.id,
     required this.senderId,
     required this.text,
+    this.type = MessageType.text,
+    this.mediaUrl,
+    this.status = MessageStatus.sent,
     required this.timestamp,
+    this.replyTo,
   });
 
-  // Pour transformer les données venant de Firestore en objet Dart
-  factory MessageModel.fromFirestore(Map<String, dynamic> data) {
+  factory MessageModel.fromFirestore(Map<String, dynamic> data, String id) {
     return MessageModel(
+      id: id,
       senderId: data['senderId'] ?? '',
       text: data['text'] ?? '',
+      type: MessageType.values.firstWhere((e) => e.toString() == data['type'], orElse: () => MessageType.text),
+      mediaUrl: data['mediaUrl'],
+      status: MessageStatus.values.firstWhere((e) => e.toString() == data['status'], orElse: () => MessageStatus.sent),
       timestamp: data['timestamp'] ?? Timestamp.now(),
+      replyTo: data['replyTo'],
     );
   }
 
-  // Pour transformer l'objet Dart en Map avant de l'envoyer à Firestore
   Map<String, dynamic> toMap() {
     return {
       'senderId': senderId,
       'text': text,
+      'type': type.toString(),
+      'mediaUrl': mediaUrl,
+      'status': status.toString(),
       'timestamp': timestamp,
+      'replyTo': replyTo,
     };
   }
 }
+
