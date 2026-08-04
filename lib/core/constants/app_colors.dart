dd2db8f3dcfa2bcf4618e1714e20a3b2d6526d7c
mauriceezechieldegnon-wq <mauriceezechieldegnon@gmail.com>
2026-08-04 14:41:59 +0100

    Refactor AppColors with new color constants

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryPurple = Color(0xFF6200EE);
-  static const Color accentYellow = Color(0xFFEEFF41);
-  static const Color backgroundBlack = Color(0xFF09090B);
-  static const Color surfaceGrey = Color(0xFF1A1A1A);
+  static const Color background = Color(0xFF09090B);
+  static const Color electricPurple = Color(0xFF6200EE);
+  static const Color neonYellow = Color(0xFFEEFF41);
+  static const Color darkCard = Color(0xFF141417);
+  static const Color metallicBorder = Color(0xFF27272A);
+  static const Color goldVIP = Color(0xFFFFD700);
+  static const Color textLight = Color(0xFFFAFAFA);
+  static const Color textMuted = Color(0xFFA1A1AA);
 }
